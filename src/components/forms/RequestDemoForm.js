2250116bb1b7921a170f37b94de464e32e1edb17
Rubi91 <rubivega91@gmail.com>
2018-06-29 12:40:10 -0500

    Fixed styles in calculator

--- src/components/forms/RequestDemoForm.js
+++ src/components/forms/RequestDemoForm.js
@@ -4,10 +4,8 @@ export class RequestDemoForm extends Component {
   render() {
     return(
       <form className="form-inline">
-        <div className="form-group mb-2">
-          <input type="text" className="input__mail"  placeholder="Your email address"/>
-          <button type="submit" className="btn btn-primary button__requestDemo">Request a demo</button>
-        </div>
+        <input type="text" className="input__mail"  placeholder="Your email address"/>
+        <button type="submit" className="btn btn-primary button__requestDemo">Request a demo</button>
       </form>
     )
   }
